1e5d39df7faa28f6ce966a52975fd9c2b113d828
ErnandoM <ernandomanoel.freire@gmail.com>
2024-06-16 15:17:31 -0300

    Adicionando as exportações

--- core_module/lib/core_module.dart
+++ core_module/lib/core_module.dart
@@ -1,6 +1,13 @@
 library core_module;
 
+export './src/notifiers/base_notifier.dart';
+export './src/shared/extensions/theme_extensions.dart';
 export './src/shared/themes/theme.dart';
 export './src/shared/themes/util.dart';
-export './src/shared/extensions/theme_extensions.dart';
+export './src/states/base_state.dart';
+export './src/shared/types/result.dart';
+export './src/entities/entity.dart';
+export './src/services/i_http_client.dart';
+export './src/exceptions/base_exception.dart';
+export 'package:fpdart/fpdart.dart';
 export 'package:flutter_modular/flutter_modular.dart';
